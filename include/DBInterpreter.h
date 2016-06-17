7f144853649d41b304ff3b89be2d5406920749d7
pdigiglio <p.digiglio91@gmail.com>
2016-06-17 14:35:57 +0200

    new header for the CallStack class

--- include/DBInterpreter.h
+++ include/DBInterpreter.h
@@ -12,6 +12,8 @@
 #include <map>
 #include <vector>
 #include <string.h>
+
+#include "CallStack.h"
 #include "Interpreter.h"
 #include "EventService.h"
 #include "ShadowThread.h"
@@ -34,44 +36,6 @@
 
 class LockMgr;
 class ThreadMgr;
-
-enum class StackAction: bool {
-	PUSH = 0,
-	POP  = 1
-};
-
-/// Simple stack wrapping a `std::vector`
-class CallStack {
-	public:
-		/// Pop a value from the top of the stack
-		const CAL_ID pop() {
-			CAL_ID call = Stack_.back();
-			Stack_.pop_back();
-			return call;
-		}
-		/// Push a value on top of the stack
-		void push(CAL_ID id) { Stack_.push_back(id); }
-
-		/// Check whether the stack is empty
-		const bool isEmpty() const { return Stack_.empty(); }
-
-		/// Top of the stack
-		/// @return The value at the top of the stack without popping it
-		const CAL_ID& top() const { return Stack_.back(); }
-
-//		StackAction update(CAL_ID parent_id) {
-//			if (isEmpty()) {
-//				push(parent_id);
-//				return StackAction::PUSH;
-//			}
-//
-//			if (top() == parent_id)
-//		}
-
-		
-	private:
-		std::vector<CAL_ID> Stack_;
-};
 
 /******************************************************************************
  * Database Interpreter
