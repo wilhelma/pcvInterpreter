9ccc3fe60911889216e487dc8784e6cebb51ae90
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2016-06-28 11:04:14 +0200

    Added documentation, edited Doxyfile to avoid json documentation

--- include/ParasiteTool.h
+++ include/ParasiteTool.h
@@ -14,9 +14,9 @@
 
 #include <array>
 #include <iostream>
-#include <unordered_map>
 #include <memory>
 #include <set>
+#include <unordered_map>
 #include <vector>
 
 #include "CallSiteProfile.h"
@@ -31,6 +31,15 @@
 #include "ShadowVar.h"
 #include "Tool.h"
 
+
+ /**
+ *
+ *    @class  ParasiteTool.h
+ *    @brief  Updates work and span variables with Events occur, to produce
+ 			  a scalability profile for the entire program as well as 
+ 			  each call site. 
+ */
+
 class ParasiteTool : public Tool {
  public:
 	ParasiteTool();
@@ -44,22 +53,63 @@ class ParasiteTool : public Tool {
 	void call(const Event* e);
 	void returnOfCalled(const Event* e);
 	void threadEnd(const Event* e);
+
+	/**
+	*    @fn printProfile()
+	*    @brief Prints overall profile, and function profile information, in a format to be decided. 
+		 @todo Decide on format to print out profile information.
+	*/
 	void printProfile();
+
+	/**
+	*    @fn getEndProfile()
+	*    @brief Converts the information contained in main_stack
+				to the end profiles for each call site in (end_call_site_profile_hashtable)
+				and the overall program (parasite_profile).
+	*/
 	void getEndProfile();
 	
-	// contains both function stack and thread stack 
+	/**
+	*    @var main_stack
+	*    @brief Contains a thread stack and a function stack to track events. 
+	*/
 	std::unique_ptr<ParasiteStack> main_stack;
 
-	// contains profile information at end of tool use 
+	/**
+	*    @var parasite_profile
+	*    @brief Contains parallelism, work, and span after simulator finishes.
+	*/
 	std::unique_ptr<parasite_profile_t> parasite_profile;
 
-	// contains profile information for each call site at end of tool use;
+	/**
+	*    @var end_call_site_profile_hashtable 
+	*    @brief Contains a detailed profile of work and span measurements
+				for each call site after simulator finishes.
+	*/
 	std::unique_ptr<call_site_end_hashtable_t> end_call_site_profile_hashtable;
 
-	// maps locks to the index in the stack of their current funciton 
+
+	/**
+	*    @var lock_hashtable
+	*    @brief Maps lock IDs to the index of their respective functions in the 
+				function stack. 
+	*/
 	std::unordered_map<unsigned int, int> lock_hashtable;
 
+	/**
+	*    @var last_strand_start_time
+	*    @brief Time stamp for the last strand that has started in the simulator.
+				A \a strand is a sequence of serially executed instructions 
+				containing no parallel control.
+	*/
 	TIME last_strand_start_time;
+
+	/**
+	*    @var total_locks_running
+	*    @brief Tracks the current number of locks running, for calculation of 
+				lock span. Lock span is only recorded when total_locks_running
+				is 1 and a lock is then released. 
+	*/
 	int total_locks_running;
 
  private:
