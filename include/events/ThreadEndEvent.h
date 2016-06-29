cb50ef58a48fdea0ce1e68e5344b6188fb8baf6b
pdigiglio <p.digiglio91@gmail.com>
2016-06-29 19:32:13 +0200

    add stub for ThreadEndEvent

---
+++ include/events/ThreadEndEvent.h
@@ -0,0 +1,37 @@
+/**
+ *
+ *    @file  ThreadEndEvent.h
+ *   @brief  
+ *
+ *    @date  06/29/16
+ *  @author  Paolo Di Giglio (github.com/pdigiglio),
+ *           <p.digiglio91@gmail.com>
+ *
+ */
+
+#ifndef  thread_end_event_H_
+#define  thread_end_event_H_
+
+#include "Event.h"
+#include "Types.h"
+
+struct ThreadEndInfo {
+};
+
+class ThreadEndEvent : public Event {
+public:
+	ThreadEndEvent(const ShadowThread *thread,
+			       const ThreadEndInfo *info)
+		: Event(thread), _info(info) {};
+	virtual Events getEventType() const override final { return Events::THREADEND; };
+	const ThreadEndInfo* getThreadEndInfo() const { return _info; };
+
+private:
+	const ThreadEndInfo* const _info;
+
+	// prevent generated functions
+	ThreadEndEvent(const ThreadEndEvent&);
+	ThreadEndEvent& operator=(const ThreadEndEvent&);
+};
+
+#endif
