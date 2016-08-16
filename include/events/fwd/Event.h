8621343cfd4a49fbd6cae7874da9b956ef538e0f
pdigiglio <p.digiglio91@gmail.com>
2016-08-16 20:44:29 +0200

    add fwd/Event.h

---
+++ include/events/fwd/Event.h
@@ -0,0 +1,17 @@
+/**
+ *
+ *    @file  Event.h
+ *   @brief  Forward header.
+ *
+ *    @date  08/16/16
+ *  @author  Paolo Di Giglio (github.com/pdigiglio),
+ *           <p.digiglio91@gmail.com>
+ *
+ */
+
+#ifndef  FWD_EVENT_H_
+#define  FWD_EVENT_H_
+
+class Event;
+
+#endif
