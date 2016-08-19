064f666a2c136aec1783d42cfc42148abcad0211
pdigiglio <p.digiglio91@gmail.com>
2016-08-19 08:02:11 +0200

    remove every file from gh-pages branch

--- ShadowLock.cpp
+++
@@ -1,9 +0,0 @@
-#include "ShadowLock.h"
-
-ShadowLock::ShadowLock(LockId lockId) : lockId(lockId) {}
-
-ShadowLock::~ShadowLock() {}
-							
-bool ShadowLock::operator < (const ShadowLock& other) const	{
-	return (lockId< other.lockId);
-}
--- ShadowThread.cpp
+++
@@ -1,7 +0,0 @@
-#include "ShadowThread.h"
-
-ShadowThread::ShadowThread(ThreadId threadId) : threadId(threadId) {}
-	
-bool ShadowThread::operator < (const ShadowThread& other) const	{
-	return (threadId< other.threadId);
-}
