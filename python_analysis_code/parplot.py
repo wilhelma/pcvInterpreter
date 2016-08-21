d7f0fac32448a586fc355642f76f5452a4e33f32
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2016-08-21 22:34:18 +0200

    added parallelism plot

--- python_analysis_code/parplot.py
+++ python_analysis_code/parplot.py
@@ -1,9 +1,21 @@
 import json
-from pprint import pprint
+import matplotlib.pyplot as plt
 
-with open('data.json') as data_file:    
+with open('parasite_output.json') as data_file:    
     data = json.load(data_file)
 
-pprint(data)
+handles = []
+
+for call_site in data["call_site_array"]:
+
+	y = call_site["parallelism"],
+	x1 = call_site["start"]
+	x2 = call_site["stop"]
+	handle = plt.plot((x1, x2), (y, y), linewidth=5, label=call_site["function_signature"])
+	handles.append(call_site["function_signature"])
+
+plt.ylim(0.8, 2.2)
+plt.legend(handles)
+plt.show()
 
 # data["maps"][0]["id"] 
