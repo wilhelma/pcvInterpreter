ed5241784a52722fdab63d984b79e8e7a166b288
pdigiglio <p.digiglio91@gmail.com>
2016-08-22 14:29:37 +0200

    improve Event.h's documentation and interface

--- include/events/Event.h
+++ include/events/Event.h
@@ -1,3 +1,5 @@
+/// @file Event.h
+
 #ifndef EVENT_H_
 #define EVENT_H_
 
@@ -8,6 +10,7 @@
 #include <string>
 #include <stdlib.h>
 
+/// @brief Run-time events.
 enum class Events: unsigned char {
     NEWTHREAD = 0x1,  // 00000001
     THREADEND = 0x2,  // 00000010
@@ -20,48 +23,80 @@ enum class Events: unsigned char {
     ALL       = 0xFF  // 11111111 
 };
 
+/// @brief Bitwise _or_ operator for `Events`.
+/// @param lhs The left-hand side.
+/// @param rhs The right-hand side.
 constexpr inline
-Events operator| (Events A, Events B) noexcept {
+Events operator|(Events lhs, Events rhs) noexcept {
     return static_cast<Events>(
-        static_cast<std::underlying_type<Events>::type>(A) |
-        static_cast<std::underlying_type<Events>::type>(B)   );
+        static_cast<std::underlying_type<Events>::type>(lhs) |
+        static_cast<std::underlying_type<Events>::type>(rhs)   );
 }
 
+/// @brief Bitwise _and_ operator for `Events`.
+/// @param lhs The left-hand side.
+/// @param rhs The right-hand side.
 constexpr inline
-Events operator& (Events A, Events B) noexcept {
+Events operator&(Events lhs, Events rhs) noexcept {
     return static_cast<Events>(
-        static_cast<std::underlying_type<Events>::type>(A) &
-        static_cast<std::underlying_type<Events>::type>(B)   );
+        static_cast<std::underlying_type<Events>::type>(lhs) &
+        static_cast<std::underlying_type<Events>::type>(rhs)   );
 }
 
+/// @brief Equality operator for `Event` and a template type.
+/// @attention The template type `T` has to support `operator==` with the
+/// underlying type of `Events`.
+/// @param  lhs The event.
+/// @tparam rhs The right-hand-side operator to be compared with the event.
+/// @bug   This shouldn't be needed! (only Events-Evenst comparison should be allowed!)
 template <typename T>
 constexpr inline
-bool operator== (Events A, T b ) noexcept {
+const bool operator==(Events lhs, T rhs) noexcept {
   return static_cast<bool>(
-        static_cast<T>(
-          static_cast<std::underlying_type<Events>::type>(A)) == b );
+        static_cast<T>(static_cast<std::underlying_type<Events>::type>(lhs)) == rhs );
 }
 
+/// @brief Equality operator for `Event` (make it commutative).
+/// @details Calls `operator==(Events, T)`.
+/// @tparam lhs The right-hand-side operator to be compared with the event.
+/// @param  rhs The event.
+/// @bug   This shouldn't be needed! (only Events-Evenst comparison should be allowed!)
 template <typename T>
 constexpr inline
-bool operator!= (Events A, T b ) noexcept {
-	return !( A == b );
+const bool operator==(T lhs, Events rhs) noexcept {
+	return rhs == lhs;
 }
 
-/// Converts a time in the format `YYYY-MM-DD|T|hh:mm:ss|.|CLOCK|Z| into 
-/// a `TIME` variable (i.e. `clock_t`)
-inline
-const TIME timeStringToTime(const TIME_STRING& t) {
-	int found = t.find_last_of('.');
-	return static_cast<TIME>(atoi(t.substr(found + 1).c_str()));
+/// @brief Unequality operator: negates `operator==(Events, T)`.
+/// @bug   This shouldn't be needed! (only Events-Evenst comparison should be allowed!)
+template <typename T>
+constexpr inline
+const bool operator!=(Events lhs, T rhs) noexcept {
+	return !(lhs == rhs);
 }
 
+/// @brief Unequality operator (make it commutative).
+/// @bug   This shouldn't be needed! (only Events-Evenst comparison should be allowed!)
+template <typename T>
+constexpr inline
+const bool operator!=(T lhs, Events rhs) noexcept {
+	return rhs != lhs;
+}
 
+///// Converts a time in the format `YYYY-MM-DD|T|hh:mm:ss|.|CLOCK|Z| into 
+///// a `TIME` variable (i.e. `clock_t`)
+//inline
+//const TIME timeStringToTime(const TIME_STRING& t) {
+//	int found = t.find_last_of('.');
+//	return static_cast<TIME>(atoi(t.substr(found + 1).c_str()));
+//}
 
-/// Abstract Event
+/// @brief Abstract Event.
+/// @details Base class for all the `*Event` classes.
 class Event {
 public:
-	Event(const ShadowThread *thread) :
+	/// Constructor.
+	explicit Event(const ShadowThread *thread) noexcept :
 		Thread_(thread) {};
 
 	/// _Default_ destructor.
@@ -71,6 +106,7 @@ public:
 	virtual Events getEventType() const = 0;
 
 private:
+	/// @todo Should this be a `unique_ptr`?
 	const ShadowThread* const Thread_;
 };
 
