8a6103bbce158bfd3e945c9705b9176cfc5777c5
pdigiglio <p.digiglio91@gmail.com>
2016-09-04 22:13:25 +0200

    add Observer structure

---
+++ include/fwd/Observer.h
@@ -0,0 +1,18 @@
+/**
+ *
+ *    @file  Observer.h
+ *   @brief  Forward header.
+ *
+ *    @date  09/04/16
+ *  @author  Paolo Di Giglio (github.com/pdigiglio),
+ *           <p.digiglio91@gmail.com>
+ *
+ */
+
+
+#ifndef  FWD_OBSERVER_H_
+#define  FWD_OBSERVER_H_
+
+struct Observer;
+
+#endif
