11e9a6b72d8da8fa6e2f393dba560ecb7df20a15
pdigiglio <p.digiglio91@gmail.com>
2016-09-06 18:15:27 +0200

    add forward for SAAPRunner

---
+++ include/fwd/SAAPRunner.h
@@ -0,0 +1,17 @@
+/**
+ *
+ *    @file  SAAPRunner.h
+ *   @brief  
+ *
+ *    @date  09/04/16
+ *  @author  Paolo Di Giglio (github.com/pdigiglio),
+ *           <p.digiglio91@gmail.com>
+ *
+ */
+
+#ifndef  FWD_SAAPRUNNER_H
+#define  FWD_SAAPRUNNER_H
+
+class SAAPRunner;
+
+#endif
