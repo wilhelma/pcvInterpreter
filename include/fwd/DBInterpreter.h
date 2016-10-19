07b13c20f9d794d5f4693560503d2ecc7e6c03c5
pdigiglio <p.digiglio91@gmail.com>
2016-10-19 16:34:12 +0200

    [DBInterpreter] add fwd header

---
+++ include/fwd/DBInterpreter.h
@@ -0,0 +1,17 @@
+/**
+ *
+ *    @file  DBInterpreter.h
+ *   @brief  Forward header.
+ *
+ *    @date  10/19/16
+ *  @author  Paolo Di Giglio (github.com/pdigiglio),
+ *           <p.digiglio91@gmail.com>
+ *
+ */
+
+#ifndef  FWD_DB_INTERPRETER_H_
+#define  FWD_DB_INTERPRETER_H_
+
+class DBInterpreter;
+
+#endif
